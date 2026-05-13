184767f96cbe31382f4dce7702725479cde3c579
FriedrichRavaDev <161723148+FriedrichRavaDev@users.noreply.github.com>
2026-05-13 12:27:37 +0200

    Remove Ordine class and StatoOrdine enum

--- 5IC-ERP/5IC-ERP/Models/Ordine.cs
+++ 5IC-ERP/5IC-ERP/Models/Ordine.cs
@@ -1,14 +1,1 @@
-﻿namespace _5IC_ERP.Models
-{
-    public class Ordine
-    {
-        public int IdOrdine { get; set; }
-        public int IdFornitore { get; set; }
-        public DateTime DataOrdine { get; set; }
-        public StatoOrdine StatoOrdine { get; set; }
-    }
-    public enum StatoOrdine
-    {
-        InElaborazione, Spedito, Consegnato
-    }
-}
+
